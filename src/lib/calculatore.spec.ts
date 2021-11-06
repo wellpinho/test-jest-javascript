302838755ea5c0ed5b86415cd3d76034b661b7e8
wellpinho <wellpinho@outlook.com>
2021-11-06 19:43:20 -0300

    NEW FEATURE: second test

--- src/lib/calculatore.spec.ts
+++ src/lib/calculatore.spec.ts
@@ -1,5 +1,27 @@
 const { sum } = require("./calculator");
 
 it('should sum 2 and 2 the result must be 4', async () => {
-  expect(sum(2,2)).toBe(4)
+  expect(sum(2, 2)).toBe(4)
+});
+
+it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
+  expect(sum('2', '2')).toBe(4)
+});
+
+it('should throw an error if what is provider to the method cannot be ', async () => {
+  expect(() => {
+    sum('', '2')
+  }).toThrowError()
+
+  expect(() => {
+    sum([2, 2])
+  }).toThrowError()
+
+  expect(() => {
+    sum({})
+  }).toThrowError()
+
+  expect(() => {
+    sum()
+  }).toThrowError()
 });
