edba510a91c12e10ff20ae76b7b55a348d439129
wellpinho <wellpinho@outlook.com>
2021-11-17 20:36:48 -0300

    restart tests

--- src/lib/calculator.js
+++
@@ -1,9 +0,0 @@
-module.exports.sum = (a, b) => {
-  const int1 = parseInt(a, 10)
-  const int2 = parseInt(b, 10)
-
-  if (Number.isNaN(int1) || Number.isNaN(int2)) {
-    throw new Error('Please check your input')
-  }
-  return +int1 + +int2;
-}
