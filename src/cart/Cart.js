ffb663b417d909f2d773b8a1c5d11765a9a12c32
wellpinho <wellpinho@outlook.com>
2021-11-24 20:49:02 -0300

    ✨ NEW FEATURE: shopping cart

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -10,27 +10,6 @@ class Cart {
       return acc + item.quantity * item.product.price
     }, 0)
   }
-  // summary() {}
-
-  // checkout() {}
 }
-
-// const cart = Cart()
-
-// const product = {
-//   title: '',
-//   price: '',
-// }
-
-// const item = {
-//   quantity: 2,
-//   product,
-// }
-
-// cart.add(item)
-// cart.remove(product)
-// cart.getTotal()
-// cart.summary()
-// cart.checkout()
 
 module.exports = { Cart }
