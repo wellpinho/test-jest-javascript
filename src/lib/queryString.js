5298dbce57d61a700e85be797fc5fdd2c6646748
wellpinho <wellpinho@outlook.com>
2022-05-21 15:26:59 -0300

    ✨ NEW FEATURE: prettier

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,4 +1,4 @@
-const queryString = (data) => {
+const queryString = data => {
   /*
     O método Object.entries() retorna uma array dos próprios pares  [key, value]
     enumeráveis de um dado objeto, na mesma ordem dos objetos providos através do
@@ -6,10 +6,10 @@ const queryString = (data) => {
     dispostas na cadeia de prototipagem - prototype chain).
   */
   const entries = Object.entries(data).map(item => {
-    return `${item[0]}=${item[1]}`
-  })
+    return `${item[0]}=${item[1]}`;
+  });
 
-  return entries.join('&')
-}
+  return entries.join('&');
+};
 
-module.exports = queryString
+module.exports = queryString;
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,12 +1,12 @@
-const queryString = require("./queryString")
+const queryString = require('./queryString');
 
 describe.only('Object query string', () => {
   it('should create a valode string when an object...', () => {
     const data = {
       name: 'well',
-      profession: 'developer'
-    }
+      profession: 'developer',
+    };
 
-    expect(queryString(data)).toBe('name=well&profession=developer')
-  })
-})
+    expect(queryString(data)).toBe('name=well&profession=developer');
+  });
+});
